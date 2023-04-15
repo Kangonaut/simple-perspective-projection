b05a72a7eb5a1f4ac1b574634349ffda57439348
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2023-04-16 00:22:11 +0200

    add cube edges

--- cube.py
+++ cube.py
@@ -6,7 +6,8 @@ from color import Colors
 
 
 class Cube:
-    def __init__(self, position: np.ndarray, size: int = 50, color: Color = Colors.GREEN, point_radius: float = 5.0):
+    def __init__(self, position: np.ndarray, size: int = 50, color: Color = Colors.GREEN, point_radius: float = 5.0,
+                 line_width: int = 1):
         self.position: np.ndarray = position
         self.points: np.ndarray = np.array([
             [-size, size, -size, 1],
@@ -21,10 +22,32 @@ class Cube:
         self.color = color
         self.point_radius: float = point_radius
         self.rotation: np.ndarray = np.array([0, 0, 0], dtype='float32')  # angle values for x, y and z
+        self.line_width: int = line_width
 
     def draw(self, surface: pygame.Surface):
+        projected_points = []
         for point in self.points:
             rotated_point = Transformation.rotation_matrix(self.rotation).dot(point)
-            point = self.position + rotated_point[:2]
-            pygame.draw.circle(surface, self.color, point[:2], self.point_radius)
-            # pygame.draw.line(surface, self.color, point[:2], neighbor_point[:2], width=1)
+            final_point = self.position + rotated_point[:2]
+
+            projected_point = final_point[:2]
+
+            projected_points.append(final_point)
+            pygame.draw.circle(surface, self.color, projected_point, self.point_radius)
+
+        # draw lines
+        for idx in range(0, 4):
+            # front square
+            start = projected_points[idx]
+            end = projected_points[(idx + 1) % 4]
+            pygame.draw.line(surface, self.color, start, end, width=self.line_width)
+
+            # back square
+            start = projected_points[idx + 4]
+            end = projected_points[((idx + 1) % 4) + 4]
+            pygame.draw.line(surface, self.color, start, end, width=self.line_width)
+
+            # connection between the squares
+            start = projected_points[idx]
+            end = projected_points[idx + 4]
+            pygame.draw.line(surface, self.color, start, end, width=self.line_width)
